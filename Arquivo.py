603b2fadf00f719c8ad6c86197fba174e8967baf
Main <lucas.andmi@gmail>
2026-04-14 09:35:53 -0300

    aula 02

--- Arquivo.py
+++ Arquivo.py
@@ -1,7 +1,7 @@
 # Aula 02 Arquivos
 # ----------------------------------------------------------------------------------------
 # abrir um arquivo para escrita
-with open('arquivo.txt', 'w', text.encode("utf-8")) as file:
+with open('arquivo.txt', 'w', encode("utf-8")) as file:
     file.write('Olá, Mundo!')
 
 # abrir um arquivo para leitura e escrita
@@ -12,3 +12,8 @@ with open ('arquivo.txt', 'r+') as file:
 # brir um arquivo para anexo
     with open('arquivo.txt', 'a') as file:
         file.write('\nMais uma linha no final do arquivo.')
+
+#abre o arqivo cno modo de escrita ('w')
+with open('arquivo.txt', 'w') as file:
+    #escreve no arquivo
+    file.write('\nlinha1')
